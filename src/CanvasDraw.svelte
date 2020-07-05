d2269f2813991fdd94b8498ca77c8f22dfa7b61a
Sharif Ahmed <me.sharifahmed@gmail.com>
2020-07-05 21:30:07 +0600

    props

---
+++ src/CanvasDraw.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let onChange = null;
+    export let loadTimeOffset = 5;
+    export let lazyRadius = 12;
+    export let brushRadius = 10;
+    export let brushColor = "#444";
+    export let catenaryColor = "#0a0302";
+    export let gridColor = "rgba(150,150,150,0.17)";
+    export let backgroundColor = "#FFF";
+    export let hideGrid = false;
+    export let canvasWidth = 400;
+    export let canvasHeight = 400;
+    export let disabled = false;
+    export let imgSrc = "";
+    export let saveData = "";
+    export let immediateLoading = false;
+    export let hideInterface = false;
+
+
+
+</script>
