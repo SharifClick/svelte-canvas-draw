4fa328373be4be39e57f4481e76c4e82a83f6bae
Sharif Ahmed <me.sharifahmed@gmail.com>
2020-07-09 20:50:53 +0600

    doc changes

--- dev/App.svelte
+++ dev/App.svelte
@@ -1,7 +1,43 @@
 <script>
-import CanvasDraw from "../src/CanvasDraw.svelte";
-let brushColor = '#444'
+  import CanvasDraw from "../src/CanvasDraw.svelte";
+  let brushColor = "#444";
+  let brushRadius = 10;
+  let uploadedimage = '';
+  let imgSrc = ''
+
+  $: {
+    imgSrc = uploadedimage
+  }
 </script>
 
-<input type="color" bind:value={brushColor}>
-<CanvasDraw {brushColor} />
+<div class="container">
+  <div class="row">
+    <div class="col-md-2 col-md-offset-5">
+        <CanvasDraw {brushColor} {brushRadius} {imgSrc} />
+
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      <div class="row">
+        <div class="col">
+
+            <div class="form-row align-items-center">
+              <div class="col-auto">
+                 <label>Brush Color</label>
+                  <input type="color" bind:value={brushColor} />
+              </div>
+              <div class="col-auto">
+                 <label>Brush Radius</label>
+                  <input type="number" bind:value={brushRadius} />
+              </div>
+              <div class="col-auto">
+                 <label>Upload an image</label>
+                  <input type="file" bind:value={uploadedimage} />
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
