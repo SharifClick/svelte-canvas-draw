37d4340e77296742a0748ecd5f8566cc8e163506
Sharif Ahmed <me.sharifahmed@gmail.com>
2020-07-19 22:59:46 +0600

    fix doc layout

--- dev/App.svelte
+++ dev/App.svelte
@@ -2,102 +2,107 @@
   import CanvasDraw from "../src/CanvasDraw.svelte";
   let brushColor = "#444";
   let brushRadius = 10;
-  let bgImage = './images/cat.png';
+  let bgImage = "./images/cat.png";
   let imgBase64 = null;
 
-
-  function setUploadedImage(e){
-
+  function setUploadedImage(e) {
     // console.log(fileUploader.files)
-    console.log(e.target.files[0])
+    console.log(e.target.files[0]);
 
-    if(e.target.files[0]) {
-        let reader = new FileReader();
-        reader.readAsDataURL(e.target.files[0]);
-        console.log(reader.result)
-        reader.onload = (ev) => {
-          // console.log(ev.target.result)
+    if (e.target.files[0]) {
+      let reader = new FileReader();
+      reader.readAsDataURL(e.target.files[0]);
+      console.log(reader.result);
+      reader.onload = ev => {
+        // console.log(ev.target.result)
         // imgBase64 = reader.result;
         imgBase64 = ev.target.result;
-        setBgImage()
-      }
+        setBgImage();
+      };
     }
   }
 
-  function setBgImage(){
+  function setBgImage() {
     bgImage = imgBase64;
   }
-
 </script>
 
 <div class="container">
   <div class="row">
-    <div class="col d-flex justify-content-center">
-  <h2>Plain draw board</h2>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col d-flex justify-content-center">
-        <CanvasDraw {brushColor} {brushRadius} />
-    </div>
-  </div>
-
-  <div class="row">
     <div class="col">
       <div class="row">
         <div class="col d-flex justify-content-center">
+          <h2>Plain draw board</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col d-flex justify-content-center">
+          <CanvasDraw {brushColor} {brushRadius} />
+        </div>
+      </div>
 
-            <div class="form-row align-items-center">
-              <div class="col-auto">
-                 <label>Brush Color</label>
+      <div class="row">
+        <div class="col">
+          <div class="row">
+            <div class="col d-flex justify-content-center">
+
+              <div class="form-row align-items-center">
+                <div class="col-auto">
+                  <label>Brush Color</label>
                   <input type="color" bind:value={brushColor} />
-              </div>
-              <div class="col-auto">
-                 <label>Brush Radius</label>
+                </div>
+                <div class="col-auto">
+                  <label>Brush Radius</label>
                   <input type="number" bind:value={brushRadius} />
+                </div>
               </div>
             </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <div class="col">
+
+      <div class="row">
+        <div class="col d-flex justify-content-center">
+          <h2>Draw board with img background</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col d-flex justify-content-center">
+          <CanvasDraw
+            {brushColor}
+            {brushRadius}
+            imgSrc={bgImage}
+            canvasWidth="640" />
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col">
+          <div class="row">
+            <div class="col d-flex justify-content-center">
+
+              <div class="form-row align-items-center">
+                <div class="col-auto">
+                  <label>Brush Color</label>
+                  <input type="color" bind:value={brushColor} />
+                </div>
+                <div class="col-auto">
+                  <label>Brush Radius</label>
+                  <input type="number" bind:value={brushRadius} />
+                </div>
+                <div class="col-auto">
+                  <label>File Upload</label>
+                  <input type="file" on:change={setUploadedImage} />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </div>
-
-
-  <hr>
-  <div class="row">
-    <div class="col d-flex justify-content-center">
-        <h2>Draw board with img background</h2>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col d-flex justify-content-center">
-        <CanvasDraw {brushColor} {brushRadius} imgSrc={bgImage} canvasWidth="640"/>
-    </div>
-  </div>
-
-  <div class="row">
-    <div class="col">
-      <div class="row">
-        <div class="col d-flex justify-content-center">
-
-            <div class="form-row align-items-center">
-              <div class="col-auto">
-                 <label>Brush Color</label>
-                  <input type="color" bind:value={brushColor} />
-              </div>
-              <div class="col-auto">
-                 <label>Brush Radius</label>
-                  <input type="number" bind:value={brushRadius} />
-              </div>
-              <div class="col-auto">
-                 <label>File Upload</label>
-                  <input type="file"  on:change={setUploadedImage}/>
-              </div>
-            </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
 
 </div>
